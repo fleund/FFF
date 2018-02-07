d0cd9a3f489d13fa3068f180e4870c64615b2cc6
Fleund <come.cothenet@hotmail.fr>
2018-02-07 23:57:50 +0100

    Spider Marche

--- FFF_Scrapy/FFF_Scrapy/spiders/spideyratebeer.py
+++ FFF_Scrapy/FFF_Scrapy/spiders/spideyratebeer.py
@@ -1,28 +1,50 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
+import logging
 
 class SpideyratebeerSpider(scrapy.Spider):
     name = 'spideyratebeer'
-    allowed_domains = ['https://www.ratebeer.com/']
-    start_urls = ['https://www.ratebeer.com/beer/kronenbourg-1664/4459/']
+    allowed_domains = ['www.ratebeer.com', 'ratebeer.com']
+    start_urls = [
+                  'https://www.ratebeer.com/beer/kronenbourg-1664/4459/'
+                  ]
 
-    def start_requests(self):
-            urls= [
-                'https://www.ratebeer.com/beer/kronenbourg-1664/4459/'       
-            ]
-            for url in urls :
-                    yield scrapy.Request(url=url, callback=self.parse)
+                  
+#    def start_requests(self):
+#            urls= [
+#                'https://www.ratebeer.com/beer/kronenbourg-1664/4459/'       
+#            ]
+#            for url in urls :
+#                    yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-            for ratebeer in response.xpath('div.ratebeer'):
-#                Modificaiton pour git
+#            reAromes=[]
+##            test = response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title').extract(),
+#            test = response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title').extract(),
+#            for i in range(len(test)):
+#                reAromes[i] = re.search(r'(?<=Aroma )\d+/\d+', test[i]).group(),
+#                print (reAromes)
+            Aromes = response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title').re(r'(?<=Aroma )\d+/\d+')
+            if Aromes is not None :
+                self.logger.info('IIIIIIIIIIIIIIIMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMIIIIIIIIIIIIIiii')
                 yield {
-                    test = response.xpath('//*[contains(concat(" ",@class," ")," reviews-container ")]//div//div//div//div//@title').extract()
-                    for i in range(len(test)):
-                        reAromes[i] = re.search(r'(?<=Aroma )\d+/\d+', test[i])
-#                    if(reAromes[i]) is None : 
-#                        reAromes[i]="NaN"
-                    
+                    'user' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small//a/text()').re('(\w+)(?=\\xa0)'),
+                    'nr_reviews' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small//a/text()').re('(?<=\\xa0\()(\d+)'),
+                    'Aromes' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title').re(r'(?<=Aroma )\d+/\d+'),
+                    'Appearance' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title').re(r'(?<=Appearance )\d+/\d+'),
+                    'Taste' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title').re(r'(?<=Taste )\d+/\d+'),
+                    'Palate' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title').re(r'(?<=Palate )\d+/\d+'),
+                    'Overall' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title').re(r'(?<=Overall )\d+/\d+'),
                 }
-                    
+            else : 
+                self.logger.info('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+                yield {
+                    'user' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small//a/text()').re('(\w+)(?=\\xa0)'),
+                    'nr_reviews' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small//a/text()').re('(?<=\\xa0\()(\d+)'),
+                    'Aromes' : 'NaN',
+                    'Appearance' : 'NaN',
+                    'Taste' : 'NaN',
+                    'Palate' : 'NaN',
+                    'Overall' : 'NaN',
+                }
