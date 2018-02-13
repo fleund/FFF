cfb47174adbb6697b78cc4d6741f417d5a7cfb28
Fleund <come.cothenet@hotmail.fr>
2018-02-13 23:48:09 +0100

    Il faut modifier pour que la spider passe par toutes les bières par page

--- FFF_Scrapy/FFF_Scrapy/settings.py
+++ FFF_Scrapy/FFF_Scrapy/settings.py
@@ -14,6 +14,7 @@ BOT_NAME = 'FFF_Scrapy'
 SPIDER_MODULES = ['FFF_Scrapy.spiders']
 NEWSPIDER_MODULE = 'FFF_Scrapy.spiders'
 
+FEED_EXPORT_ENCODING = "utf-8"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'FFF_Scrapy (+http://www.yourdomain.com)'
--- FFF_Scrapy/FFF_Scrapy/spiders/kronenbourg-1664_4459.py
+++ FFF_Scrapy/FFF_Scrapy/spiders/kronenbourg-1664_4459.py
@@ -8,6 +8,18 @@ class KronenbourgSpider(scrapy.Spider):
     allowed_domains = ['www.ratebeer.com', 'ratebeer.com']
     start_urls = [
                   'https://www.ratebeer.com/beer/kronenbourg-1664/4459/1/1/',
+                  'https://www.ratebeer.com/beer/kronenbourg-(original)/4457/1/1/',
+                    'https://www.ratebeer.com/beer/kronenbourg-1664-blanc/35424/1/1/',
+                    'https://www.ratebeer.com/beer/leffe-blonde/2514/1/1/',
+                    'https://www.ratebeer.com/beer/leffe-tripel-(triple)/2515/1/1/',
+                    'https://www.ratebeer.com/beer/leffe-9%C2%B0-%2F-rituel-9/56545/1/1/',
+                    'https://www.ratebeer.com/beer/leffe-ruby/91345/1/1/',
+                    'https://www.ratebeer.com/beer/la-chouffe/1614/1/1/',
+                    'https://www.ratebeer.com/beer/heineken/37/1/1/',
+                    'https://www.ratebeer.com/beer/bavaria-8.6-(original)/5588/1/1/',
+                    'https://www.ratebeer.com/beer/kellegen-extra-forte-8.0/107281/1/1/',
+                    'https://www.ratebeer.com/beer/davelghem-blonde/117003/1/1/',
+                    'https://www.ratebeer.com/beer/fischer-tradition/9760/1/1/',
                   ]
     page_suiv=1
 
@@ -20,27 +32,27 @@ class KronenbourgSpider(scrapy.Spider):
 #                    yield scrapy.Request(url=url, callback=self.parse)
     
     def parse(self, response):
-
+            for i in range(10):
                 yield {
                             'beer_name' : response.xpath('//div[contains(concat(" ", @class, " "), " user-header ")]/h1/a/span/text()').extract_first(),
-                            'user' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small//a/@href').re('(?<=/user/)\d+'),
-                            'localisation' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small').re('(?<=\ - ).+(?= -)'),
-                            'time_review' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small').re('(?<= - )\w+ \d+, \d+(?=</small>)'),
-                            'nr_reviews' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small//a/text()').re('(?<=\\xa0\()(\d+)'),
-                            'score' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div/div/@title').extract_first().strip('<br />'),
-                            'comment' :response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]/div/div/div/text()').extract(),
+                            'user' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small//a/@href').re_first('(?<=/user/)\d+'),
+                            'localisation' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small').re_first('(?<=\ - ).+(?= -)'),
+                            'time_review' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small').re_first('(?<= - )\w+ \d+, \d+(?=</small>)'),
+                            'nr_reviews' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//small//a/text()').re_first('(?<=\\xa0\()(\d+)'),
+                            'score' : response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div/div/@title').extract_first(),
+#                            'comment' :response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]/div/div/div/text()').extract(),
                             
                     }
                                 
-                for notation in response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title') :
-                            yield {
-                                        'note' :notation.re_first(r'(\d out of 5\.0|\d\.\d out of 5\.0)'),
-                                        'Aromes' : notation.re_first(r'(?<=Aroma )\d+/\d+'),
-                                        'Appearance' : notation.re_first(r'(?<=Appearance )\d+/\d+'),
-                                        'Taste' : notation.re_first(r'(?<=Taste )\d+/\d+'),
-                                        'Palate' : notation.re_first(r'(?<=Palate )\d+/\d+'),
-                                        'Overall' : notation.re_first(r'(?<=Overall )\d+/\d+'),
-                                   }
+#                for notation in response.xpath('//*[contains(concat(" ", @class, " "), " reviews-container ")]//div//div//div//div//@title') :
+#                            yield {
+#                                        'note' :notation.re_first(r'(\d out of 5\.0|\d\.\d out of 5\.0)'),
+#                                        'Aromes' : notation.re_first(r'(?<=Aroma )\d+/\d+'),
+#                                        'Appearance' : notation.re_first(r'(?<=Appearance )\d+/\d+'),
+#                                        'Taste' : notation.re_first(r'(?<=Taste )\d+/\d+'),
+#                                        'Palate' : notation.re_first(r'(?<=Palate )\d+/\d+'),
+#                                        'Overall' : notation.re_first(r'(?<=Overall )\d+/\d+'),
+#                                   }
                                    
                                    #yield {
 #                            'beer_name' : response.xpath('//div[contains(concat(" ", @class, " "), " user-header ")]/h1/a/span/text()').extract_first(),
@@ -56,13 +68,14 @@ class KronenbourgSpider(scrapy.Spider):
 #                        }                    
                            
                 
-                page_act = re.search(r'(?<=\d/)\d+(?=/>$)', str(response)).group()
-                page_suiv = str(int(page_act) +1)
-                next_page = response.urljoin(str(page_suiv)+'/')
+            page_act = re.search(r'(?<=\d/)\d+(?=/>$)', str(response)).group()
+            page_suiv = str(int(page_act) +1)
+            next_page = response.urljoin(str(page_suiv)+'/')
+            
+            next_page = next_page[:-(len(str(page_suiv))+len(str(page_act))+2)]+next_page[-(len(str(page_suiv))+1):]
+            self.logger.info(next_page)
                 
-                next_page = next_page[:-(len(str(page_suiv))+len(str(page_act))+2)]+next_page[-(len(str(page_suiv))+1):]
-                self.logger.info(next_page)
-                
-                if int(page_suiv) <= int(response.xpath('//*[contains(concat(" ", @class, " "), " ballno ")]/text()').extract()[-1]):
-                    yield response.follow(next_page, callback=self.parse)
+            if int(page_suiv)<=3:                
+#            if int(page_suiv) <= int(response.xpath('//*[contains(concat(" ", @class, " "), " ballno ")]/text()').extract()[-1]):
+                yield response.follow(next_page, callback=self.parse)
                 
